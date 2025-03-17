63a9a6abd29b00c72d4f1cd97ac1067bbfef16f2
Cong13ING <15577490429@163.com>
2025-03-17 18:24:34 +0800

    update name

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,12 +3,12 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
+export const name = 'cong13Ing'
+export const headline = 'Student at GUET'
 export const introduction =
-  "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
-export const email = 'tong.hu@scls-sh.org'
-export const githubUsername = 'derekhut'
+  "这是介绍"
+export const email = '15577490429@163.com'
+export const githubUsername = 'cong13Ing'
 
 // about page
 export const aboutMeHeadline = 'Who Are You and Why Should I Care?'
@@ -40,7 +40,7 @@ export const socialLinks: Array<SocialLinkType> = [
   {
     name: 'Bilibili',
     icon: 'bilibili',
-    href: 'https://space.bilibili.com/349721082',
+    href: 'https://space.bilibili.com/382507799/upload/video',
   },
 ]
 
